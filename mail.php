8b46ba66096f7abd7f89367ec688ae06c5d877f5
neswill1 <nenadkonatar@live.ac.me>
2015-04-23 13:31:28 +0200

    Email address changed

--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@ if(isset($_POST['mail'])){
     }
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "Natasha@NaplesMedSpa.Com";
+    $email_to = "Pam@NaplesMedSpa.Com";
     $email_subject = "Information from the MOBILE contact form!";      
 
     // validation expected data exists
