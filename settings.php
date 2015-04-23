7102c873dca872615e4a8e28f794711d7c7a519d
neswill1 <nenadkonatar@live.ac.me>
2015-04-23 10:57:06 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+$adresa="898 5th Avenue South Suite 204 Naples, FL 34102"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad="Naples";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+$telephone= 2392287491;// Broj koji se poziva na dugme CALL US
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
